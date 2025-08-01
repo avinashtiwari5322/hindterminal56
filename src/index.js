2762f4462c740ec995dac7ea0e5c34e6381a6ec0
nikhilsahu2002 <death1233freak@gmail.com>
2025-08-01 11:43:06 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://hindterminalfrontend.vercel.app/"],
+    origin: ["http://localhost:3000", "https://hindterminalfrontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
